fa2d76a587d4daef1a1376d5bea602b44166ecd1
Gelmi <ggelmisalvo@gmail.com>
2023-12-18 12:29:46 -0300

    adding fabric api template

---
+++ src/client/java/com/gelmi/copperpp/CopperClient.java
@@ -0,0 +1,10 @@
+package com.gelmi.copperpp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopperClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
